d090132ae5a499c4c5168ac2092193a6c6067a98
Milos Zivadinovic <milos@ignispotentia.com>
2020-01-09 14:25:35 +0100

    First commit

---
+++ BlazorToDo/Services/CardModelService.cs
@@ -0,0 +1,74 @@
+﻿using BlazorToDo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorToDo.Services
+{
+    public class CardModelService
+    {
+        private List<CardModel> cards = new List<CardModel>()
+        {
+            new CardModel
+            {
+                Id = 0,
+                Title = "Feed the chickens",
+                Content = "They get hungry",
+                Done = false
+            },
+            new CardModel
+            {
+                Id = 1,
+                Title = "Water your plants",
+                Content = "Lush green pineapples",
+                Done = true
+            },
+            new CardModel
+            {
+                Id = 2,
+                Title = "Get food",
+                Content = "Pizza should be fine",
+                Done = false
+            },
+            new CardModel
+            {
+                Id = 3,
+                Title = "Wash the dishes",
+                Content = "Otherwise you'll eat from the floor",
+                Done = false
+            },
+        };
+
+        public Task<List<CardModel>> GetCards()
+        {
+            Task<List<CardModel>> result = null;
+
+            try
+            {
+                result = Task.FromResult(cards);
+            }
+            catch
+            {
+            }
+
+            return result;
+        }
+
+        public bool DeleteCard(int id)
+        {
+            bool result = false;
+
+            try
+            {
+                cards.RemoveAll(c => c.Id == id);
+                result = true;
+            }
+            catch
+            {
+            }
+
+            return result;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Blazor ToDo application
+=======================
+
+I really don't like web development, but I wanted to try out Blazor
+
+The focus on the syntax and the code, not on functionalities. That's why you can't add new ToDo cards.
